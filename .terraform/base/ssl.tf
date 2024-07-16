4ad43cbd93205d00411ebfe9016b189de51d678e
Tim Kallady <t.kallady@garvan.org.au>
2024-07-16 17:31:12 +1000

    cert name change

--- .terraform/base/ssl.tf
+++ .terraform/base/ssl.tf
@@ -1,6 +1,6 @@
 resource "google_compute_managed_ssl_certificate" "lb_default" {
   provider = google-beta
-  name     = "seqr-${var.env}-ssl-cert"
+  name     = "seqr-${var.env}-ssl-cert-2"
 
   managed {
     domains = ["${var.subdomain}.dsp.garvan.org.au"]
